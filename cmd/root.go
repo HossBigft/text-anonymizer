5b867adb28ceeeca6d8355c5b666b52b5dc367ad
Maxim Gluschenko <ma.gluschenko@timeweb.ru>
2026-03-01 16:07:13 +0500

    Move functionality to root func

--- cmd/root.go
+++ cmd/root.go
@@ -28,7 +28,71 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	Run: func(cmd *cobra.Command, args []string) {},
+	Run: func(cmd *cobra.Command, args []string) {
+		// s := bufio.NewScanner(os.Stdin)
+		// for s.Scan() {
+		// 	fmt.Println("line", s.Text())
+		// }
+
+		IPV4_REGEX := `(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}`
+		FQDN_REGEX := `(?:[_a-z0-9](?:[_a-z0-9-]{0,61}[a-z0-9])?\.)+(?:[a-z](?:[a-z0-9-]{0,61}[a-z0-9])?)`
+		configDir := filepath.Join(os.Getenv("HOME") + "/.config/anonymizer/")
+		maskedValuesFilePath := os.Getenv("HOME") + "/.config/anonymizer/map.json"
+		masksPatternsFilePath := os.Getenv("HOME") + "/.config/anonymizer/maskPatterns.json"
+		maskPatterns, err := loadPatterns(masksPatternsFilePath)
+		isMaskPatternsUpdated := false
+		if err != nil {
+			maskPatterns = append(maskPatterns, MaskPattern{Name: "ipv4", Regex: IPV4_REGEX})
+			maskPatterns = append(maskPatterns, MaskPattern{Name: "fqdn", Regex: FQDN_REGEX})
+			isMaskPatternsUpdated = true
+		}
+		file, err := os.Open("examples/nginx_access.log")
+		check(err)
+		defer file.Close()
+
+		valuesToMasks := make(map[string]string)
+		maskedValuesFileHandle, err := os.ReadFile(maskedValuesFilePath)
+		err = json.Unmarshal(maskedValuesFileHandle, &valuesToMasks)
+
+		scanner := bufio.NewScanner(file)
+		var isMasksUpdated bool
+		for scanner.Scan() {
+			line := scanner.Text()
+			replaced_line := line
+			for _, pattern := range maskPatterns {
+				regex, _ := regexp.Compile(pattern.Regex)
+				sensitive_value := regex.FindString(line)
+				if len(sensitive_value) != 0 {
+					mask, present := valuesToMasks[sensitive_value]
+					if present {
+						replaced_line = strings.ReplaceAll(replaced_line, sensitive_value, mask)
+					} else {
+						mask, _ = reggen.Generate(pattern.Regex, 7)
+						valuesToMasks[sensitive_value] = mask
+						replaced_line = strings.ReplaceAll(replaced_line, sensitive_value, mask)
+						isMasksUpdated = true
+					}
+				}
+
+			}
+			fmt.Println(replaced_line)
+		}
+
+		if isMasksUpdated {
+			err = os.MkdirAll(configDir, 0755)
+			valueMapJson, err := json.Marshal(valuesToMasks)
+			check(err)
+			err = os.WriteFile(maskedValuesFilePath, valueMapJson, 0644)
+			check(err)
+		}
+		if isMaskPatternsUpdated {
+			err = os.MkdirAll(configDir, 0755)
+			valueMapJson, err := json.Marshal(maskPatterns)
+			check(err)
+			err = os.WriteFile(masksPatternsFilePath, valueMapJson, 0644)
+			check(err)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -56,66 +120,9 @@ func loadPatterns(path string) ([]MaskPattern, error) {
 	return patterns, err
 }
 func Execute() {
-	IPV4_REGEX := `(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}`
-	FQDN_REGEX := `(?:[_a-z0-9](?:[_a-z0-9-]{0,61}[a-z0-9])?\.)+(?:[a-z](?:[a-z0-9-]{0,61}[a-z0-9])?)`
-	configDir := filepath.Join(os.Getenv("HOME") + "/.config/anonymizer/")
-	maskedValuesFilePath := os.Getenv("HOME") + "/.config/anonymizer/map.json"
-	masksPatternsFilePath := os.Getenv("HOME") + "/.config/anonymizer/maskPatterns.json"
-	maskPatterns, err := loadPatterns(masksPatternsFilePath)
-	isMaskPatternsUpdated := false
-	if err != nil {
-		maskPatterns = append(maskPatterns, MaskPattern{Name: "ipv4", Regex: IPV4_REGEX})
-		maskPatterns = append(maskPatterns, MaskPattern{Name: "fqdn", Regex: FQDN_REGEX})
-		isMaskPatternsUpdated = true
-	}
-	err = rootCmd.Execute()
-	file, err := os.Open("examples/nginx_access.log")
-	check(err)
-	defer file.Close()
 
-	valuesToMasks := make(map[string]string)
-	maskedValuesFileHandle, err := os.ReadFile(maskedValuesFilePath)
-	err = json.Unmarshal(maskedValuesFileHandle, &valuesToMasks)
-
-	scanner := bufio.NewScanner(file)
-	var isMasksUpdated bool
-	for scanner.Scan() {
-		line := scanner.Text()
-		replaced_line := line
-		for _, pattern := range maskPatterns {
-			regex, _ := regexp.Compile(pattern.Regex)
-			sensitive_value := regex.FindString(line)
-			if len(sensitive_value) != 0 {
-				mask, present := valuesToMasks[sensitive_value]
-				if present {
-					replaced_line = strings.ReplaceAll(replaced_line, sensitive_value, mask)
-				} else {
-					mask, _ = reggen.Generate(pattern.Regex, 7)
-					valuesToMasks[sensitive_value] = mask
-					replaced_line = strings.ReplaceAll(replaced_line, sensitive_value, mask)
-					isMasksUpdated = true
-				}
-			}
-
-		}
-		fmt.Println(replaced_line)
-	}
-
-	if isMasksUpdated {
-		err = os.MkdirAll(configDir, 0755)
-		valueMapJson, err := json.Marshal(valuesToMasks)
-		check(err)
-		err = os.WriteFile(maskedValuesFilePath, valueMapJson, 0644)
-		check(err)
-	}
-	if isMaskPatternsUpdated {
-		err = os.MkdirAll(configDir, 0755)
-		valueMapJson, err := json.Marshal(maskPatterns)
-		check(err)
-		err = os.WriteFile(masksPatternsFilePath, valueMapJson, 0644)
-		check(err)
-	}
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
