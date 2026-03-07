8c307532d0d68f3efeece5bcd00a433215e6d52b
Maxim Gluschenko <ma.gluschenko@timeweb.ru>
2026-03-08 00:58:00 +0500

    Add flag and code to unmask text

--- cmd/root.go
+++ cmd/root.go
@@ -26,6 +26,15 @@ func mask(rawLine string, patternManager patternmanager.PatternManager, maskMana
 
 	return replaced_line
 }
+func unmask(rawLine string, maskManager maskmanager.MaskManager) string {
+	replaced_line := rawLine
+	masks := maskManager.GetReverseMap()
+	for sensitive_value, mask := range masks {
+		replaced_line = strings.ReplaceAll(replaced_line, sensitive_value, mask)
+	}
+
+	return replaced_line
+}
 
 var filePath string
 var rootCmd = &cobra.Command{
@@ -40,6 +49,8 @@ to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		maskManager := maskmanager.NewMaskManager()
 		patternManager := patternmanager.NewPatternManager()
+		reverse, _ := cmd.Flags().GetBool("reverse")
+
 		if len(filePath) > 0 {
 			file, err := os.Open(filePath)
 			check(err)
@@ -48,7 +59,13 @@ to quickly create a Cobra application.`,
 			scanner := bufio.NewScanner(file)
 			for scanner.Scan() {
 				line := scanner.Text()
-				replaced_line := mask(line, *patternManager, *maskManager)
+				var replaced_line string
+				if reverse {
+					replaced_line = unmask(line, *maskManager)
+				} else {
+					replaced_line = mask(line, *patternManager, *maskManager)
+				}
+
 				fmt.Println(replaced_line)
 			}
 		} else {
@@ -58,14 +75,24 @@ to quickly create a Cobra application.`,
 					scanner := bufio.NewScanner(os.Stdin)
 					for scanner.Scan() {
 						line := scanner.Text()
-						replaced_line := mask(line, *patternManager, *maskManager)
+						var replaced_line string
+						if reverse {
+							replaced_line = unmask(line, *maskManager)
+						} else {
+							replaced_line = mask(line, *patternManager, *maskManager)
+						}
 						fmt.Println(replaced_line)
 					}
 				}
 			} else {
 				for _, val := range strings.Split(args[0], "\n") {
 					line := val
-					replaced_line := mask(line, *patternManager, *maskManager)
+					var replaced_line string
+					if reverse {
+						replaced_line = unmask(line, *maskManager)
+					} else {
+						replaced_line = mask(line, *patternManager, *maskManager)
+					}
 					fmt.Println(replaced_line)
 				}
 
@@ -90,4 +117,5 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().StringVarP(&filePath, "file", "f", "", "File to process")
+	rootCmd.Flags().BoolP("reverse", "r", false, "Unmask masked text")
 }
